682111a9caf73a62a9dfd0c66755ee414c3e61e7
zhangdi-devel <zhangdi.devel@gmail.com>
2018-02-13 01:14:18 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ echo "compiling done $(date)"
 if [[ ! -f src/target/scala-2.12/donation_analytics.jar ]]
 then
 	echo "compiling failed? will use the pre-compiled jar file $(date)"
-	java -Xmx4g -jar src/target/scala-2.12/donation_analytics_fallback.jar $@
+	java -Xmx6g -jar src/target/scala-2.12/donation_analytics_fallback.jar $@
 else
 	echo "start running $(date)"
-	java -Xmx4g -jar src/target/scala-2.12/donation_analytics.jar $@
+	java -Xmx6g -jar src/target/scala-2.12/donation_analytics.jar $@
 fi
 echo "done $(date)"
